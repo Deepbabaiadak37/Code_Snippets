297df86baf2963e5c00dfd636afc3aa1d4ab9584
unknown <deekumaradak@gmail.com>
2022-01-08 23:45:54 +0530

    day 11

---
+++ Recursive_Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main
+{
+    static int getFibo(int n)
+    {
+        if(n==1 || n==0)
+            return n;
+        else
+            return getFibo(n-1)+getFibo(n-2);
+    }
+    
+	public static void main(String[] args) 
+	{
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print(getFibo(n));
+	}
+}

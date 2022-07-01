a006f557cb5da6cff23505e2b6007250ae179985
Deepbabaiadak37 <deekumaradak@gmail.com>
2022-07-01 12:51:11 +0530

    1/7

---
+++ First_bad.java
@@ -0,0 +1,17 @@
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n - 1;       
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+              
+        }
+        return start;
+    }
+}

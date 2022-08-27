87dd7484832edbbe96539026982317a3e2703265
Deepbabaiadak37 <deekumaradak@gmail.com>
2022-08-27 23:00:44 +0530

    27/8

---
+++ Missing_Numbers_In_Array.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n *(n + 1) / 2;
+
+        for(int num: nums){
+            sum-=num;
+        }
+
+        return sum;
+    }
+}

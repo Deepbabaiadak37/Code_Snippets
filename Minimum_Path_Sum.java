1acb07d8a3e516f1849345bfbdfb4d2ad16c3394
Deepbabaiadak37 <deekumaradak@gmail.com>
2022-09-02 23:09:33 +0530

    2/9

--- Minimum_Path_Sum.java
+++ Minimum_Path_Sum.java
@@ -26,3 +26,33 @@ public class Solution {
         return f(grid.length-1,grid[0].length-1,grid,dp);
     }
 }
+
+
+
+
+//Another Same Solution
+
+
+class Solution {
+    public int f(int i,int j,int[][] grid,int[][] dp)
+    {
+        if(i==0 && j==0) return grid[i][j];
+        if(i<0 || j<0) return (int)1e9;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int up=grid[i][j]+f(i-1,j,grid,dp);
+        int left=grid[i][j]+f(i,j-1,grid,dp);
+        
+        return dp[i][j]=Math.min(up,left);
+        
+    }
+    public int minPathSum(int[][] grid) {
+        
+        int[][] dp=new int[grid.length][grid[0].length];
+        for(int[] row: dp)
+            Arrays.fill(row,-1);
+        
+        return f(grid.length-1,grid[0].length-1,grid,dp);
+    }
+}

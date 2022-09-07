956e26a269fe4350eb5371283f5efe4d380a9051
Deepbabaiadak37 <deekumaradak@gmail.com>
2022-09-07 23:43:51 +0530

    7/9

---
+++ Check_If_Sentence_Is_Panagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if (sentence.length() < 26) {
+			return false;
+		}
+		String alphas = "abcdefghijklmnopqrstuvwxyz";
+		for (int i = 0; i < alphas.length(); i++) {
+			if (sentence.indexOf(alphas.charAt(i)) == -1) {
+				return false;
+			}
+		}
+		return true;
+    }
+}

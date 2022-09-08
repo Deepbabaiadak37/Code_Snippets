bf19a1d366cb64fee908e267a3b2395d7404a348
Deepbabaiadak37 <deekumaradak@gmail.com>
2022-09-08 20:35:00 +0530

    8/9

---
+++ Construct_()String_From_Binary_Tree.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    
+    public String tree2str(TreeNode t)
+    {
+        StringBuilder sb = new StringBuilder();
+        helper(sb,t);
+        return sb.toString();
+    }
+    
+    public void helper(StringBuilder sb,TreeNode t)
+    {
+        if( t==null) return;
+      
+        sb.append(t.val);
+        
+        if(t.left!=null||t.right!=null)
+        {
+            sb.append("(");
+            helper(sb,t.left);
+            sb.append(")");
+            if(t.right!=null)
+            {
+                sb.append("(");
+                helper(sb,t.right);
+                sb.append(")");
+            }
+        }
+        
+    }
+}
